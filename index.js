0af9c6def113da6af4ca68811efca03951adf5d4
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-19 11:14:53 -0400

    commit tree b4488689cd9b183a601673c543a9b582c7de2a80

--- index.js
+++ index.js
@@ -20,3 +20,83 @@ let allWagesFor = function () {
 
     return payable
 }
+
+var createEmployeeRecord = function(employee_info){
+    //const employeeObject = new Object();
+    let newObj = {
+    firstName: employee_info[0],
+    familyName: employee_info[1],
+    title: employee_info[2],
+    payPerHour: employee_info[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+    }
+    return newObj
+}
+
+function createEmployeeRecords(arrays) {
+    const array1 = arrays.map(createEmployeeRecord);
+    return array1
+}
+
+function createTimeInEvent(timeStamp) {
+    let dateSplit = timeStamp.split(' ');
+    let timeDetails = new Object();
+        timeDetails.type = "TimeIn";
+        timeDetails.hour = parseInt(dateSplit[1]);
+        timeDetails.date = dateSplit[0];
+    
+    this.timeInEvents.push(timeDetails)
+    return this
+}
+
+function createTimeOutEvent(timeStamp) {
+    let dateSplit = timeStamp.split(' ');
+    let timeDetails = new Object();
+        timeDetails.type = "TimeOut";
+        timeDetails.hour = parseInt(dateSplit[1]);
+        timeDetails.date = dateSplit[0];
+    
+    this.timeOutEvents.push(timeDetails)
+    return this
+}
+
+function hoursWorkedOnDate(dateTime) {
+    let clockIn = this.timeInEvents.find(function(d) {
+       return d.date === dateTime
+    })
+
+    let clockOut = this.timeOutEvents.find(function(d) {
+        return d.date === dateTime
+     })
+    let timeWorked = clockOut.hour - clockIn.hour;
+    return timeWorked / 100;
+}
+
+function wagesEarnedOnDate(dateTime) {
+    let hoursWorked = hoursWorkedOnDate.call(this, dateTime);
+
+    return this.payPerHour * hoursWorked
+}
+
+function calculatePayroll(employeeArray) {
+    let payRollTotal = employeeArray.reduce(function(accumulator, employeeObject) {
+        return allWagesFor(employeeObject) + accumulator;
+    }, 0);
+    return payRollTotal
+}
+
+function calculatePayroll(employeeArray) {
+    let payRollTotal = employeeArray.reduce(function(accumulator, employeeObject) {
+        return allWagesFor.call(employeeObject) + accumulator;
+    }, 0);
+    return payRollTotal
+}
+
+
+function findEmployeeByFirstName(employeeArray, name1) {
+    let nameMatch =  employeeArray.find( function(n) {
+        return n.firstName === name1
+    })
+    return nameMatch
+}
